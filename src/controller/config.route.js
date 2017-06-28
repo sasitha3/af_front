1d6ec25863d765506cdf16471c7215c836882382
sasitha3 <sasitha.premadasa@my.sliit.lk>
2017-06-28 21:31:13 +0530

    routing added for orders

--- src/controller/config.route.js
+++ src/controller/config.route.js
@@ -6,14 +6,40 @@ console.log("Sasitha ");
 
 myApp.config(['$routeProvider', function ($routeProvider) {
 
-    $routeProvider.when('/home', {
-        templateUrl : 'orders/home.html',
-        controller : 'requestOrderController'
-    })
+    $routeProvider.
+        when('/home', {
+            templateUrl : 'orders/home.html',
+            controller : 'requestOrderController'
+        })
         .when('/directory', {
             templateUrl : 'orders/directory.html',
             controller : 'orderController'
-        }).otherwise({
+        })
+        .when('/email', {
+            templateUrl : 'orders/email.html',
+            controller : 'emailController'
+        })
+        .when('/requestList', {
+            templateUrl : 'orders/requestList.html',
+            controller : 'emailController'
+        })
+        .when('/orderList', {
+            templateUrl : 'orders/orderList.html',
+            controller : 'emailController'
+        })
+        .when('/rejectList', {
+            templateUrl : 'orders/rejectList.html',
+            controller : 'emailController'
+        })
+        .when('/diliverList', {
+            templateUrl : 'orders/diliverList.html',
+            controller : 'emailController'
+        })
+        .when('/messages', {
+            templateUrl : 'orders/messages.html',
+            controller : 'emailController'
+        })
+        .otherwise({
         redirectTo : '/home',
         controller : 'orderController'
     });
